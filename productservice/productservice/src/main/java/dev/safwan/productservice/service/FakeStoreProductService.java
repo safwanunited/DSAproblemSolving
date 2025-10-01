f7ecd1baf704c8b698de9e429eb777dcae97fbe5
safwanTW <mohammed.safwanjs@thoughtworks.com>
2025-10-01 12:06:54 +0530

    commenting spring cloud dependency

--- productservice/productservice/src/main/java/dev/safwan/productservice/service/FakeStoreProductService.java
+++ productservice/productservice/src/main/java/dev/safwan/productservice/service/FakeStoreProductService.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Primary
 @Service("fakeStoreProductService")
 public class FakeStoreProductService implements ProductService {
 
